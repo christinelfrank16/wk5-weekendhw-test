0b4c77c80987ef2b68eae24730a013193578b814
Christine Frank <christine.braun13@gmail.com>
2019-09-02 10:25:30 -0700

    Add clean and uglify webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,12 +10,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-  new HtmlWebpackPlugin({
-    title: 'Ping Pong',
-    template: './src/index.html',
-    inject: 'body'
-  })
-],
+    new UglifyJsPlugin(),  
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
